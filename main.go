ece7708c985ce4f48a8565a4148c286aa22fa80f
EliweltonMoreira <eliwelton.moreira@outlook.com>
2022-03-05 20:14:09 -0300

    add connection with db

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/internal/config"
 	"api/internal/router"
 	"fmt"
 	"log"
@@ -8,8 +9,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Executing API!")
+	config.Load()
 	r := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	fmt.Printf("Listening on port %d", config.APIPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.APIPort), r))
 }
