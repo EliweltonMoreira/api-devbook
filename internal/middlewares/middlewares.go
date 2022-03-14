ee54626f50630498dd353a9f979a2cc7be8c3153
EliweltonMoreira <eliwelton.moreira@outlook.com>
2022-03-14 20:55:29 -0300

    add token validation

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -1,23 +1,27 @@
 package middlewares
 
 import (
-	"fmt"
+	"api/internal/authentication"
+	"api/internal/responses"
 	"log"
 	"net/http"
 )
 
 // Logger writes request info in the terminal
-func Logger(next http.HandlerFunc) http.HandlerFunc {
+func Logger(nextFunction http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		nextFunction(w, r)
 	}
 }
 
 // Authenticate verify if the user making the request is authenticated
-func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+func Authenticate(nextFunction http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Authenticating...")
-		next(w, r)
+		if err := authentication.ValidateToken(r); err != nil {
+			responses.Error(w, http.StatusUnauthorized, err)
+			return
+		}
+		nextFunction(w, r)
 	}
 }
